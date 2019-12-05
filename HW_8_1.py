d63eaa492aefbbf0cbdc9c5330997db74044a620
Ivan Kralj <ivan.kralj0511@gmail.com>
2019-12-05 19:33:40 +0100

    initial commit

---
+++ HW_8_1.py
@@ -0,0 +1,17 @@
+mood = input ("Please enter your mode: ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Take a beer!")
+elif mood == "excited":
+    print("slow down")
+elif mood == "relaxed":
+    print ("turn on TV")
+else:
+    print ("don't recognize this mod")
+
+
+
